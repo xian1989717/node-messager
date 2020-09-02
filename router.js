eaeff65e7f740420e1b973d64357ef66994eed60
刘述 <383883040@qq.com>
2020-09-02 15:15:39 +0800

    提交代码

--- router.js
+++ router.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const {
   getIndex,
-  getOne
+  getOne,
+  getEditOne
 } = require('./service')
+
 const router = express.Router()
 
 router
   .get('/', getIndex)
   .get('/getOne', getOne)
+  .get('/editOne', getEditOne)
 
 module.exports = router
--- service.js
+++ service.js
@@ -15,9 +15,22 @@ module.exports = {
     const sql = 'select * from node_messager where Mno= ' + query.id
     connection.query(sql, (err, data) => {
       const _data = {
-        list: data 
+        list: data
       }
       res.render('detail.html', _data)
+    })
+  },
+  getEditOne (req, res) {
+    const query = req.query
+    const sql = 'select * from node_messager where Mno=' + query.id
+    connection.query(sql, (err, data) => {
+      if (err) {
+        res.send('error')
+      }
+      const _data = {
+        list: data
+      }
+      res.render('edit.html', _data)
     })
   }
 }
